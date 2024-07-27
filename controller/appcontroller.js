f83de5ecf01a6aacd8424504cb65fcac9069277f
shankar kumar <shankarkum20@gmail.com>
2024-07-27 17:25:43 +0530

    first commit

--- controller/appcontroller.js
+++ controller/appcontroller.js
@@ -44,11 +44,11 @@ const sendmess=async(req,res)=>{
     const {email,message,name}=req.body;
 
     if (!email || !message || !name){
-       return res.status(205).json({
+       return res.status(200).json({
         success:false,
         msg:"Please Enter all field"
        })
-    }
+    };
       let config={
        service:'gmail',
        auth:{
